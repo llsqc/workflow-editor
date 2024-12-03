b18c7dca3658de25860efca01ab6a617b7fdbff9
univero <1449610641@qq.com>
2024-12-03 18:51:06 +0800

    feat: 补充了agent的type字段，避免分表

--- entity/agent/agent.py
+++ entity/agent/agent.py
@@ -1,7 +1,10 @@
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, IntField
 
 """
 agent父类，定义了name，description，avatar的基本属性
+type-0 analyser
+type-1 judge
+type-2 hook
 """
 
 
@@ -9,3 +12,4 @@ class Agent(Document):
     name = StringField()
     description = StringField()
     avatar = StringField()
+    type = IntField()
